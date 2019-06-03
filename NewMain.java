9e4beb488882602de0dfffa9a99b4c9d02a1a48f
jmartinezpineiro <jmartinezpineiro@nautilus16.danielcastelao.org>
2019-06-03 10:55:17 +0200

    Boletin terminado

---
+++ NewMain.java
@@ -0,0 +1,12 @@
+package src;
+/**
+ * @author jmartinezpineiro
+ */
+public class NewMain {
+    public static void main(String[] args) {
+        
+            Vent v = new Vent();
+            v.setVisible(true);
+
+    }
+}
